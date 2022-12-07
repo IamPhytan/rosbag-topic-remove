def6911e9ce3f901dfa4636ba6cc8592f180d566
Damien LaRocque <phicoltan@gmail.com>
2022-12-07 11:53:32 -0500

    Implement topic filterer

--- src/rosbag_topic_remove/topic_remover.py
+++ src/rosbag_topic_remove/topic_remover.py
@@ -9,8 +9,10 @@ from rosbags.rosbag1 import Reader as Reader1
 from rosbags.rosbag2 import Reader as Reader2
 from tqdm import tqdm
 
+import fnmatch
+
 if TYPE_CHECKING:
-    from typing import Sequence
+    from typing import Sequence, Tuple
 
 
 class BagTopicRemover:
@@ -33,25 +35,41 @@ class BagTopicRemover:
             raise ValueError(f"{value} is not an existing file")
 
     @staticmethod
-    def filter_out_topics(bag_topics: Sequence[str], topics_to_remove: Sequence[str]):
+    def filter_out_topics(
+        bag_topics: Sequence[str], patterns_to_remove: Sequence[str]
+    ) -> Tuple[str]:
         """Filter out topics
 
         Examples:
-        >>> bag_topics = ("/cmd_vel", "/imu/data", "/imu/data_raw", "/imu/odom", "/lidar_packets", "/map", "/velocity")
-        >>> to_filter = ("/imu/*", "/lidar_packets")
-        >>> filter_out_topics(bag_topics, to_filter)
-        ("/cmd_vel", "/map", "/velocity")
-        >>> to_filter = ("/cmd_vel", "/map", "/velocity")
-        >>> filter_out_topics(bag_topics, to_filter)
-        ("/imu/data", "/imu/data_raw", "/imu/odom", "/lidar_packets")
-        >>> bag_topics = ("/imu/data", "/imu/data_raw", "/imu/odom")
-        >>> to_filter = ("/camera/image_raw")
-        >>> filter_out_topics(bag_topics, to_filter)
-        ("/imu/data", "/imu/data_raw", "/imu/odom")
+        >>> bag_topics = ('/cmd_vel', '/imu/data', '/imu/data_raw', '/imu/odom', '/lidar_packets', '/map', '/velocity')
+        >>> to_filter = ('/imu/*', '/lidar_packets')
+        >>> BagTopicRemover.filter_out_topics(bag_topics, to_filter)
+        ('/cmd_vel', '/map', '/velocity')
+        >>> to_filter = ('/cmd_vel', '/map', '/velocity')
+        >>> BagTopicRemover.filter_out_topics(bag_topics, to_filter)
+        ('/imu/data', '/imu/data_raw', '/imu/odom', '/lidar_packets')
+        >>> bag_topics = ('/imu/data', '/imu/data_raw', '/imu/odom')
+        >>> to_filter = ('/camera/image_raw')
+        >>> BagTopicRemover.filter_out_topics(bag_topics, to_filter)
+        ('/imu/data', '/imu/data_raw', '/imu/odom')
 
         Args:
             bag_topics: input rosbag's topics
-            topics_to_remove: topics to filter out
-        """
+            patterns_to_remove: topics to filter out
 
-        # fnmatch.fnmatch("/camera", "/imu/*") => True
+        Returns:
+            tuple: Filtered topics that were not targeted by the pattern
+        """
+        # Accumulate a list of topics
+        topics_to_remove = []
+        for pattern in patterns_to_remove:
+            pattern_topics = fnmatch.filter(bag_topics, pattern)
+            topics_to_remove.extend(pattern_topics)
+
+        # Keep only one copy of each element in the list
+        topics_to_remove = tuple(set(topics_to_remove))
+
+        filtered_topics = tuple(
+            topic for topic in bag_topics if topic not in topics_to_remove
+        )
+        return filtered_topics
